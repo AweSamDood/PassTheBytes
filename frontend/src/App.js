cea746e7d980345146df29b15a4030a3c3c4325d
AweSamDude <samo.benko@gmail.com>
2025-01-16 16:25:57 +0100

    fix(darkmode): now in local storage for persistence

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import {ConfigProvider, theme} from 'antd';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { ConfigProvider, theme } from 'antd';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Files from './pages/Files';
 import './App.css';
 import PublicSharePage from "./pages/ShareFilePage";
 
-
 function App() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        // Retrieve the theme preference from localStorage
+        const savedTheme = localStorage.getItem('isDarkMode');
+        return savedTheme ? JSON.parse(savedTheme) : false;
+    });
 
     const toggleTheme = () => {
-        setIsDarkMode((prev) => !prev);
+        setIsDarkMode((prev) => {
+            const newTheme = !prev;
+            // Save the theme preference to localStorage
+            localStorage.setItem('isDarkMode', JSON.stringify(newTheme));
+            return newTheme;
+        });
     };
 
     useEffect(() => {
@@ -36,10 +44,10 @@ function App() {
         >
             <Router>
                 <Routes>
-                    <Route path="/" element={<Login/>}/>
+                    <Route path="/" element={<Login />} />
                     <Route
                         path="/files"
-                        element={<Files toggleTheme={toggleTheme} isDarkMode={isDarkMode}/>}
+                        element={<Files toggleTheme={toggleTheme} isDarkMode={isDarkMode} />}
                     />
                     <Route path="/share/:shareKey" element={<PublicSharePage />} />
                 </Routes>
